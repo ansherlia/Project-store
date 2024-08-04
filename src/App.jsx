29efdf187ab40f2ec62e598b950119a8a102a5ee
reza hagju <ansherlireza2020@gmail.com>
2024-08-04 22:20:57 +0330

    Make SPA Routes and Pages Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProductsPage from "./Pages/ProductsPage";
+import Checkout from "./Pages/Checkout";
+import ProductDetailsPage from "./Pages/ProductDetailsPage";
+import PageNotFound from "./Pages/404";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<Routes>
+			<Route index element={<Navigate to="/products" replace />} />
+			<Route path="/products" element={<ProductsPage />} />
+			<Route path="/products/:id" element={<ProductDetailsPage />} />
+			<Route path="checkout" element={<Checkout />} />
+			<Route path="*" element={<PageNotFound />} />
+		</Routes>
+	);
 }
 
-export default App
+export default App;
