ab974e46c76693f605a50ca2c0921fa66036c969
reza hagju <ansherlireza2020@gmail.com>
2024-08-07 12:52:39 +0330

    added Layouts to project and do style to that

---
+++ src/Layouts/Layout.jsx
@@ -0,0 +1,34 @@
+/** @format */
+import { FaCartPlus } from "react-icons/fa6";
+import { FaGrinHearts } from "react-icons/fa";
+
+import { useCart } from "../contexts/CardProvider";
+import styles from "./Layout.module.css";
+import { Link } from "react-router-dom";
+function Layout({ children }) {
+	const [state] = useCart();
+	const { itemCounter } = state;
+	console.log(itemCounter);
+	return (
+		<div className={styles.container}>
+			<header>
+				<Link to={"/products"}>TaskMan Store</Link>
+				<button>
+					<Link to={"/checkout"} className={styles.basketLink}>
+						<FaCartPlus />
+						{!!itemCounter && <span>{itemCounter}</span>}
+					</Link>
+				</button>
+			</header>
+			{children}
+			<footer>
+				Developed By Reza with{" "}
+				<span>
+					<FaGrinHearts />
+				</span>
+			</footer>
+		</div>
+	);
+}
+
+export default Layout;
