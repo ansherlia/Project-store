655a70196f573b2c07cc6e8efcd2b0ed43c4f62b
reza hagju <ansherlireza2020@gmail.com>
2024-08-07 19:30:23 +0330

    added InfoBasketBuy to checkout component and take a style

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import BasketBuy from "../components/BasketBuy";
+import InfoBasketBuy from "../components/InfoBasketBuy";
 import { useCart } from "../contexts/CardProvider";
 import styles from "./Checkout.module.css";
 function Checkout() {
@@ -9,13 +10,14 @@ function Checkout() {
 		dispatch({ type, payload });
 	};
 	return (
-		<>
+		<div className={styles.checkout}>
+			<InfoBasketBuy />
 			<div className={styles.container}>
 				{state.selectedItems.map((p) => (
 					<BasketBuy data={p} key={p.id} clickHandler={clickHandler} />
 				))}
 			</div>
-		</>
+		</div>
 	);
 }
 
